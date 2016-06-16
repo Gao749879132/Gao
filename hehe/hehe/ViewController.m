b5395b63fdc90d65f2696b284340ba6430c6ada6
Gao749879132 <749879132@qq.com>
2016-06-16 11:55:22 +0800

    1231

---
+++ hehe/hehe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hehe
+//
+//  Created by apple on 16/6/16.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"asdasdasd");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
